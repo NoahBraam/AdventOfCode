db7cccecc5f589abfcb0695fc79d600c985edc2c
Noah Braam <nwbraam@gmail.com>
2025-12-03 22:32:24 -0500

    day3 done

--- y2025/src/day3/03.py
+++ y2025/src/day3/03.py
@@ -1,16 +1,29 @@
-total = 0
 
-with open('input.txt', 'r') as f:
-    for line in f:
-        largest = '0'
-        second = '0'
-        for c in line[:-2]:
-            if int(c) > int(largest):
-                largest = c
-        start = line.index(largest) + 1
-        for c in line[start:-1]:
-            if int(c) > int(second):
-                second = c
-        total += int(largest + second)
+def main():
+    num_batteries = int(input())
+    max_joltages = []
+    with open('input.txt', 'r') as f:
+        input_data = f.read().splitlines()
+    
+    for bank in input_data:
+        bank_len = len(bank)
+        bank_joltages = [int(joltage) for joltage in bank]
+        remaining = num_batteries
+        batteries_enabled = []
+        i = -1 
+    
+        while remaining > 0:
+            search_slice = bank_joltages[i+1 : bank_len - remaining + 1]
+            battery_joltage = max(search_slice)
+            
+            i = bank_joltages.index(battery_joltage, i+1)
+            
+            batteries_enabled.append(battery_joltage)
+            batteries_remaining -= 1
 
-print(total)
+        max_joltages.append(int("".join(str(battery) for battery in batteries_enabled)))
+    print(sum(max_joltages))
+
+
+if __name__ == "__main__":
+    main()
