717eab470b89549bb678ee4d253636d4d7b7436e
NoahBraam <nwbraam@gmail.com>
2025-12-06 18:04:46 -0500

    day 6 part 1

---
+++ y2025/src/day6/06.py
@@ -0,0 +1,31 @@
+def main():
+    with open("input.txt", "r") as f:
+        input_data = f.read().splitlines()
+
+    numbers = []
+    operation = []
+    part1 = 0
+    for i in range(len(input_data)):
+        if i == len(input_data) - 1:
+            operation = [opts for opts in input_data[i].split(" ") if opts != ""]
+        else:
+            numbers.append([int(num) for num in input_data[i].split(" ") if num != ""])
+
+
+    for i in range(len(operation)):
+        total = 0
+        if operation[i] == "*":
+            total = 1
+
+        for j in range(len(numbers)):
+            if operation[i] == "*":
+                total *= numbers[j][i]
+            elif operation[i] == "+":
+                total += numbers[j][i]
+        part1 += total
+    print(part1)
+
+
+
+if __name__ == "__main__":
+    main()
